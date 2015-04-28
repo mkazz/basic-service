9176054039a265edd65c67ae796a4b23cee376ff
matt <matthewkaczorowski@gmail.com>
2015-04-27 20:28:24 -0400

    WIP

--- src/Controllers/VehicleController.php
+++ src/Controllers/VehicleController.php
@@ -2,13 +2,14 @@
 
 namespace Mworx\VehicleService\Controllers;
 
+use Mworx\VehicleService\Entities\Vehicle;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class VehicleController {
 
     protected $vehicle;
 
-    public function __construct() {
+    public function __construct(Vehicle $vehicle) {
     }
 
     public function findById($id = 99) {
--- src/webroot/index.php
+++ src/webroot/index.php
@@ -5,4 +5,3 @@ require_once __DIR__ . "/../../vendor/autoload.php";
 $app = require __DIR__ . "/../app.php";
 $app->run();
 
-
