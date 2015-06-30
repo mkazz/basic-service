a79814a802dba851b9148223a442844c783275dd
matt <matthewkaczorowski@gmail.com>
2015-06-30 16:44:17 -0400

    Added Error Message to base dao

--- src/DAOs/BaseDAO.php
+++ src/DAOs/BaseDAO.php
@@ -6,11 +6,16 @@ class BaseDAO {
 
     protected 
         $entity,
-        $dbal;
+        $dbal,
+        $error;
 
     public function __construct($dbal, $entity) {
         $this->dbal   = $dbal;
         $this->entity = $entity;
+    }
+
+    public function getError() {
+        return $this->error;
     }
 }
 
