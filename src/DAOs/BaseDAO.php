a529bd36c5c760017c515d30d60b20f50df17d4b
matt <matthewkaczorowski@gmail.com>
2015-07-03 11:13:08 -0400

    Adding unit tests

--- src/DAOs/BaseDAO.php
+++ src/DAOs/BaseDAO.php
@@ -2,6 +2,8 @@
 
 namespace MKaczorowski\BasicService\DAOs;
 
+use MKaczorowski\BasicService\Entities;
+
 class BaseDAO {
 
     protected 
@@ -17,5 +19,52 @@ class BaseDAO {
     public function getError() {
         return $this->error;
     }
+
+    protected function __get($name) {
+        switch ($name) {
+            case "qb":
+                return $this->dbal->createQueryBuilder();
+            default:
+                return null;
+        }
+    }
+
+    protected function fetchAssoc(Entities\BaseEntity $entity, $query, $params = []) {
+        try {
+            $result = $this->dbal->fetchAssoc($query, $params);
+            if (!empty($result)) {
+                $entity->load($result);
+                return $entity;
+            }
+
+            return null;
+        } catch (\Exception $e) {
+            $this->error = $e->getMessage();
+        }
+
+        return false;
+    }
+
+    protected function fetchAll(Entities\BaseEntity $entity, $query, $params = []) {
+        try {
+            $entities = [];
+            $result = $this->dbal->fetchAll($query, $params);
+            if (!empty($result)) {
+                $class_name = get_class($entity);
+                foreach ($result as $row) {
+                    $entity = new $class_name();
+                    $entity->load($row);
+                    $entities[] = $entity;
+                }
+            }
+
+            return $entities;
+        } catch (\Exception $e) {
+            $this->error = $e->getMessage();
+        }
+
+        return false;
+    }
+
 }
 
---
+++ tests/unit/DAOs/BaseDAOTest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace MKaczorowski\BasicService\Tests\DAOs;
+
+class BaseDAOTest extends PHPUnit_Framework_TestCase {
+}
+
